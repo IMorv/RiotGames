d8e43f3d7d36ee34bb96c69229e2821a6f2cfe27
IMorv <109215930+IMorv@users.noreply.github.com>
2022-12-15 11:34:19 -0300

    recomecei o projeto, parei no msm lugar
    Não consigo ativar o input no js, estou frustrado mas ainda vou persistir, nao sei o que tem de errado, tentei varias vezes arrumar e nao achei o problema, vou parar por agora pois me estressou bastante, estou decepcionado comigo.

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
-var inputs = document.querySelectorAll('.input');
-var handleFocus = (event) => {
-    console.log(event.target)
+const inputs = document.querySelectorAll('.input');
+const button = document.querySelector('.login_button');
+
+const handleFocus = ({ target }) => {
+  const span = target.previousElementSibling;
+  span.classList.add('span-active');
 }
 
-inputs.forEach((input) =>
-    input.addEventListener('focus', handleFocus))
+  inputs.forEach((input) => input.addEventListener('focus', handleFocus))
