9d9ce8c6f18451ee6f08da5df0c0635dc2000f46
IMorv <109215930+IMorv@users.noreply.github.com>
2022-12-31 21:03:59 -0300

    consegui finalmente fazer o site

--- script.js
+++
@@ -1,9 +0,0 @@
-const inputs = document.querySelectorAll('.input');
-const button = document.querySelector('.login_button');
-
-const handleFocus = ({ target }) => {
-  const span = target.previousElementSibling;
-  span.classList.add('span-active');
-}
-
-  inputs.forEach((input) => input.addEventListener('focus', handleFocus))
